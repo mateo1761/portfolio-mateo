5031ee48a41333c28afe1fd3efc5c4beb335e21f
Mateo Quintero Zapata <mateoquinterozapata@gmail.com>
2026-08-04 19:53:57 -0500

    🐛 Add readable project validation messages

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Requests;
 
+use App\Concerns\ProjectValidationRules;
 use App\Models\Project;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateProjectRequest extends FormRequest
 {
+    use ProjectValidationRules;
+
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -26,19 +29,6 @@ class UpdateProjectRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'title_es' => ['required', 'string', 'max:255'],
-            'title_en' => ['required', 'string', 'max:255'],
-            'category_es' => ['required', 'string', 'max:255'],
-            'category_en' => ['required', 'string', 'max:255'],
-            'description_es' => ['required', 'string', 'max:5000'],
-            'description_en' => ['required', 'string', 'max:5000'],
-            'technologies_es' => ['required', 'string', 'max:255'],
-            'technologies_en' => ['required', 'string', 'max:255'],
-            'repository_url' => ['nullable', 'url:http,https', 'max:2048'],
-            'is_private' => ['required', 'boolean'],
-            'is_published' => ['required', 'boolean'],
-            'sort_order' => ['required', 'integer', 'min:0', 'max:32767'],
-        ];
+        return $this->projectRules();
     }
 }
